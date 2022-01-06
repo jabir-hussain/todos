42dfdbf22b7b6a618f6daf24e8a1d4f13fbbf04c
jabir <jabirhussainturi@gmail.com>
2022-01-06 12:04:33 +0500

    basic todos app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import { useQuery } from "react-query";
+
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
 
 function App() {
+  const { data: todos, isLoading } = useQuery("todos", () => {
+    return fetch("/todos")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data", data);
+        return data;
+      });
+  });
+
+  if (isLoading) return <div>Loading....</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul className="App">
+      {todos.map((todo) => {
+        return <li key={todo.id}>{todo.title}</li>;
+      })}
+    </ul>
   );
 }
 
